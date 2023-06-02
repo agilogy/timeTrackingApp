0c4bf000b36c81b267e6581a896fd87dbf3558b3
Daniel Alonso <daniel.alonso@agilogy.com>
2023-06-02 12:33:09 +0200

    Replace instructions to prepare test environment

--- components/postgresdb/src/test/kotlin/com/agilogy/timetracking/drivenadapters/postgresdb/TimeEntriesRepositoryTest.kt
+++ components/postgresdb/src/test/kotlin/com/agilogy/timetracking/drivenadapters/postgresdb/TimeEntriesRepositoryTest.kt
@@ -25,10 +25,6 @@ import javax.sql.DataSource
 
 class TimeEntriesRepositoryTest : FunSpec() {
 
-    // To execute test in local:
-    // docker pull postgres
-    // docker run --name postgres_timetrackingapp -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres
-
     private suspend fun <A> withTestDataSource(database: String? = "test", f: suspend (DataSource) -> A) =
         HikariCp.dataSource(
             "jdbc:postgresql://localhost:5432/${database ?: ""}",
