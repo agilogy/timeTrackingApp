88d4645f4d3e81e94b099be65344bf68f26ca909
manelp <manel.perez@intenthq.com>
2023-06-09 13:49:46 +0200

    Throws TogglInvalidTimeEntry when time entry is invalid

--- components/toggl/src/main/kotlin/com/agilogy/timetracking/toggl/TogglImporter.kt
+++ components/toggl/src/main/kotlin/com/agilogy/timetracking/toggl/TogglImporter.kt
@@ -13,14 +13,18 @@ import java.time.temporal.ChronoUnit
 
 fun readTogglCsv(inputStream: InputStream, zoneId: ZoneId): List<Triple<ProjectName, ClosedRange<Instant>, ZoneId>> =
     csvReader().readAllWithHeader(inputStream).map { row ->
-        val projectName = ProjectName(row.getValue("Project"))
-        val startDate = LocalDate.parse(row.getValue("Start date"))
-        val startTime = LocalTime.parse(row.getValue("Start time"))
-        val parsedDuration = LocalTime.parse(row.getValue("Duration"))
+        try {
+            val projectName = ProjectName(row.getValue("Project"))
+            val startDate = LocalDate.parse(row.getValue("Start date"))
+            val startTime = LocalTime.parse(row.getValue("Start time"))
+            val parsedDuration = LocalTime.parse(row.getValue("Duration"))
 
-        val startInstant = LocalDateTime.of(startDate, startTime).atZone(zoneId).toInstant()
+            val startInstant = LocalDateTime.of(startDate, startTime).atZone(zoneId).toInstant()
 
-        val duration = Duration.of(parsedDuration.toSecondOfDay().toLong(), ChronoUnit.SECONDS)!!
-        val range = startInstant..startInstant.plus(duration)
-        Triple(projectName, range, zoneId)
+            val duration = Duration.of(parsedDuration.toSecondOfDay().toLong(), ChronoUnit.SECONDS)!!
+            val range = startInstant..startInstant.plus(duration)
+            Triple(projectName, range, zoneId)
+        } catch (e: Exception) {
+            throw TogglInvalidTimeEntry(e)
+        }
     }
