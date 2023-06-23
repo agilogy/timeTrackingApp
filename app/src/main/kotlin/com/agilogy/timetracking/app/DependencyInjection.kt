890dae9835bb2fcc22a932ccbb805f3b273aab50
Adrià Mañes Medina <amanesmedina@gmail.com>
2023-06-23 13:42:58 +0200

    Read Heroku db env variable

--- app/src/main/kotlin/com/agilogy/timetracking/app/DependencyInjection.kt
+++ app/src/main/kotlin/com/agilogy/timetracking/app/DependencyInjection.kt
@@ -4,9 +4,15 @@ import arrow.fx.coroutines.ResourceScope
 import com.agilogy.db.hikari.HikariCp
 import com.agilogy.timetracking.domain.TimeTrackingAppPrd
 import com.agilogy.timetracking.drivenadapters.postgresdb.PostgresTimeEntriesRepository
+import java.net.URI
 
 suspend fun ResourceScope.timeTrackingApp(): TimeTrackingAppPrd {
-    val dataSource = HikariCp.dataSource("jdbc:postgresql://localhost/test", "postgres", "postgres").bind()
+    val dbUri = URI(System.getenv("DATABASE_URL"))
+
+    val (username, password) = dbUri.userInfo.split(":")
+    val jdbcUrl = "jdbc:postgresql://" + dbUri.host + ':' + dbUri.port + dbUri.path
+
+    val dataSource = HikariCp.dataSource(jdbcUrl, username, password).bind()
     val repo = PostgresTimeEntriesRepository(dataSource)
     return TimeTrackingAppPrd(repo)
 }
