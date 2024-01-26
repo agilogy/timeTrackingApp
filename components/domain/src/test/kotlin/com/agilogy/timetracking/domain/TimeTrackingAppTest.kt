5e7398b8add0f3ca2e8ba55aced55a25707a2930
Jose Raya <jose.raya@agilogy.com>
2024-01-26 12:54:09 +0100

    adds some tests for the domain layer

--- components/domain/src/test/kotlin/com/agilogy/timetracking/domain/TimeTrackingAppTest.kt
+++ components/domain/src/test/kotlin/com/agilogy/timetracking/domain/TimeTrackingAppTest.kt
@@ -4,6 +4,7 @@ import com.agilogy.timetracking.domain.test.InMemoryTimeEntriesRepository
 import io.kotest.core.spec.style.FunSpec
 import org.junit.jupiter.api.Assertions.assertEquals
 import java.time.Instant
+import java.time.LocalDate
 import java.time.ZoneId
 
 class TimeTrackingAppTest : FunSpec() {
@@ -16,10 +17,15 @@ class TimeTrackingAppTest : FunSpec() {
         val zoneId: ZoneId = ZoneId.of("Australia/Sydney")
 
         test("Save time entries") {
+            // ARRANGE
             val timeEntriesRepository = InMemoryTimeEntriesRepository()
             val app = TimeTrackingAppPrd(timeEntriesRepository)
             val developerTimeEntries = listOf(Triple(project, start..now, zoneId))
+
+            // ACT
             app.saveTimeEntries(developer, developerTimeEntries)
+
+            // ASSERT
             val expected = listOf(TimeEntry(developer, project, start..now, zoneId))
             assertEquals(expected, timeEntriesRepository.getState())
         }
@@ -41,7 +47,48 @@ class TimeTrackingAppTest : FunSpec() {
             assertEquals(expected, result)
         }
 
-        // TODO: Test the other methods of the app
+        test("Get hours per developer ignores entries outside the time range") {
+            val timeEntriesRepository = InMemoryTimeEntriesRepository(
+                listOf(
+                    TimeEntry(
+                        developer,
+                        project,
+                        start..now,
+                        zoneId,
+                    ),
+                    TimeEntry(
+                        developer,
+                        project,
+                        start.minusSeconds(3600 * 24)..now.minusSeconds(3600 * 24),
+                        zoneId,
+                    ),
+                ),
+            )
+            val app = TimeTrackingAppPrd(timeEntriesRepository)
+            val result = app.getDeveloperHours(start..now)
+            val expected = mapOf((developer to project) to Hours(hours))
+            assertEquals(expected, result)
+        }
+
+        test("Get developer hours by project and date") {
+            val timeEntriesRepository = InMemoryTimeEntriesRepository(
+                listOf(
+                    TimeEntry(
+                        developer,
+                        project,
+                        start..now,
+                        zoneId,
+                    ),
+                ),
+            )
+            val app = TimeTrackingAppPrd(timeEntriesRepository)
+            var range = LocalDate.now()..LocalDate.now()
+
+            val result = app.getDeveloperHoursByProjectAndDate(developer, range)
+
+            val expected = listOf(Triple(LocalDate.now(), project, Hours(hours)))
+            assertEquals(expected, result)
+        }
 
         // TODO: Specially test the logic in listTimeEntries
     }
