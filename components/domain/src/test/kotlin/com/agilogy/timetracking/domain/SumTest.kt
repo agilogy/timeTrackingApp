7523619ab726d63a11d41dc7f682bd688c35a221
Jordi Pradel <jordi.pradel@agilogy.com>
2024-05-03 15:22:41 +0200

    Practice PBT with sums

--- components/domain/src/test/kotlin/com/agilogy/timetracking/domain/SumTest.kt
+++ components/domain/src/test/kotlin/com/agilogy/timetracking/domain/SumTest.kt
@@ -5,8 +5,9 @@ import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
 import io.kotest.property.checkAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 
-fun sum(a: Int, b: Int): Int = a + b
+fun sum(a: Int, b: Int): Int = a + b // if (a == 0) b - 1 else a + b
 
 class SumTest : FunSpec() {
     init {
@@ -19,10 +20,13 @@ class SumTest : FunSpec() {
 
         test("sum is associative") {
             // for all a, b, c: a + (b + c) = (a + b) + c
-            TODO()
+            checkAll(Arb.int(), Arb.int(), Arb.int()) { a, b, c ->
+                assertEquals(sum(a, sum(b, c)), sum(sum(a, b), c))
+            }
         }
 
         test("sum is commutative") {
+            // for all a, b: a+b = b+a
             checkAll(Arb.int(), Arb.int()) { a, b ->
                 assertEquals(sum(a, b), sum(b, a))
             }
@@ -30,7 +34,15 @@ class SumTest : FunSpec() {
 
         test("sum has 0 as a neutral element") {
             // for all a: a + 0 = 0 + a = a
-            TODO()
+            checkAll(Arb.int()) { a ->
+                assertEquals(a, sum(a, 0))
+            }
+        }
+
+        test("foo") {
+            checkAll(Arb.int(-1000..1000), Arb.int(1..1000)) { a, b ->
+                assertTrue(sum(a, b) > a)
+            }
         }
     }
 }
